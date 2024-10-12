e56701404c994850b611a9377ad2a67401263b72
FloreDenis2001 <floredenis907@yahoo.com>
2024-10-12 14:16:48 +0300

    update

---
+++ src/main/java/com/example/tecnosserver/exceptions/exception/AlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.example.tecnosserver.exceptions.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class AlreadyExistsException extends AppException {
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/tecnosserver/user/service/UserQueryServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.tecnosserver.user.service;
+
+import com.example.tecnosserver.exceptions.exception.NotFoundException;
+import com.example.tecnosserver.user.model.User;
+import com.example.tecnosserver.user.repo.UserRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserQueryServiceImpl implements UserQueryService {
+    private final UserRepo userRepo;
+
+
+    public UserQueryServiceImpl(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        Optional<User> user = userRepo.findByEmail(email);
+        if (user.isPresent()) {
+            return user;
+        } else {
+            throw new NotFoundException("User with email " + email + " not found");
+        }
+    }
+}
