54ba9243d2406eda266aec42c005356bbf8b6163
FloreDenis2001 <floredenis907@yahoo.com>
2024-10-14 23:17:18 +0300

    created-repo-for-category-done

---
+++ src/main/java/com/example/tecnosserver/category/repo/CategoryRepo.java
@@ -0,0 +1,15 @@
+package com.example.tecnosserver.category.repo;
+
+import com.example.tecnosserver.category.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface CategoryRepo extends JpaRepository<Category, Long> {
+    
+    Optional<Category> findCategoryByName(String name);
+
+}
